fae01ea7604eaeb50944da5ec05b0d0bf17c19c1
Monica Erdman <merdman@usgs.gov>
2018-10-14 21:23:23 -0700

    Finish problem #2

--- homework/Erdman_hw1.py
+++ homework/Erdman_hw1.py
@@ -2,6 +2,7 @@
 #Homework 1
 #Python for programmers
 
+#Problem 1
 def kaprekar(x):
     x_str = '{0:04d}'.format(x)
     x_sortUp = ''.join(sorted(x_str))
@@ -23,3 +24,18 @@ def kap_iter(x):
 run1 = kap_iter(8730)
 
 run2 = kap_iter(9730)
+
+#Problem 2
+str1 = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW' 
+#12W1B12W3B24W1B14W
+count = 1
+compress = ''
+for i in range(1, len(str1)):
+    letter = str1[i]
+    if str1[i-1] == letter:
+        count +=1
+    else:
+        compress += str(count) + str(letter)
+        count = 1
+compress += str(count) + str(letter) #encode the final char
+print(compress)
